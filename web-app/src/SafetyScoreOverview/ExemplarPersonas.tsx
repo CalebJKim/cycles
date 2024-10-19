bb6ac66f3686f2f7e4cf9a3e84ee16a0db7ecd19
Steven Segawa <stevenseg288@gmail.com>
2024-10-19 15:35:24 -0700

    Scrollable Personas

--- web-app/src/SafetyScoreOverview/ExemplarPersonas.tsx
+++ web-app/src/SafetyScoreOverview/ExemplarPersonas.tsx
@@ -1,14 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Persona from './Persona';
 import '../App.css'
 
+const personaData = [
+    { 
+      imageSrc: "https://i.imgur.com/2alOrqT.jpeg",
+      title: "Cool Cat 1",
+      description: "This cat knows what's up!"
+    },
+    {
+      imageSrc: "https://i.imgur.com/2alOrqT.jpeg",
+      title: "Cool Cat 2",
+      description: "I love cats"
+    },
+    {
+      imageSrc: "https://i.imgur.com/2alOrqT.jpeg",
+      title: "Cool Cat 3",
+      description: "Wait there's even more :O"
+    }
+  ];
+
 const ExemplarPersonas: React.FC = () => {
   return (
-    <div className="ExemplarPersonas">
-        <Persona
-            imageSrc={require('./cat.jpg')} 
-            title="Cool Cat"
-            description="This cat knows what's up!" />
+    <div className="scroll-container">
+      {
+        personaData.map((persona, index) => (
+          <div key={index} className="persona-wrapper">
+            <Persona
+              id={index}
+              imageSrc={persona.imageSrc}
+              title={persona.title}
+              description={persona.description}
+            />
+          </div>
+        ))
+      }
     </div>
   );
 }
--- web-app/src/SafetyScoreOverview/Persona.tsx
+++ web-app/src/SafetyScoreOverview/Persona.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../App.css'; // Import your CSS for styling
 
-const Persona = ({ imageSrc, title, description }: { imageSrc: string; title: string; description: string }) => {
+const Persona = ({ id, imageSrc, title, description }: { id: number; imageSrc: string; title: string; description: string }) => {
   return (
     <div className="info-card">
       <div className="info-card-image">
         <img src={imageSrc} alt={title} />
       </div>
       <div className="info-card-text">
-        <h2>{title}</h2>
+        <h2>{id}: {title}</h2>
         <p>{description}</p>
       </div>
     </div>
